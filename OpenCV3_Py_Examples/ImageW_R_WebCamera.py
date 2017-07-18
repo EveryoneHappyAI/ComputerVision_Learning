3cb5faf88298309088be1eef1b2112ae14ba91d4
ChopinNocture <chopin_7up@icloud.com>
2017-07-19 04:25:22 +0800

    交换蓝红通道测试

--- OpenCV3_Py_Examples/ImageW_R_WebCamera.py
+++ OpenCV3_Py_Examples/ImageW_R_WebCamera.py
@@ -15,27 +15,29 @@ import numpy as np
 
 from skimage import io
 
-image = cv2.imread("C:\\Users\\006\\Pictures\\tx.jpg")
+fileStr = "C:\\Users\\yj_7u\\Pictures\\Icon.bmp"
+#fileStr = "C:\\Users\\006\\Pictures\\tx.jpg"
+
+image = cv2.imread(fileStr)
 
 testImage = image.copy()
 
-a = testImage[:,:,2].copy();
-testImage[:,:,2] = testImage[:,:,0]
-testImage[:,:,0] = a 
-
+testImage[:,:,2] ^= testImage[:,:,0]
+testImage[:,:,0] ^= testImage[:,:,2] 
+testImage[:,:,2] ^= testImage[:,:,0]
 
 print(image[0])
 print("----------------------------------------------")
 print(testImage[0])
 
-#io.imshow(image)
+io.imshow(image)
 
 io.imshow(testImage)
 
 
 
-cv2.imshow("TestImg", image)
-cv2.waitKey()
-cv2.imshow("TestImg", testImage)
-cv2.waitKey()
-cv2.destroyAllWindows()
+#cv2.imshow("TestImg", image)
+#cv2.waitKey()
+#cv2.imshow("TestImg", testImage)
+#cv2.waitKey()
+#cv2.destroyAllWindows()
