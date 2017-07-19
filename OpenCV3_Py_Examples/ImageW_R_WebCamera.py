3e21c1cca4b8fbfd1eaac85525abf8a560a192d9
ChopinNocture <chopin_7up@icloud.com>
2017-07-19 17:22:52 +0800

    传至 surface 测试摄像头功能

--- OpenCV3_Py_Examples/ImageW_R_WebCamera.py
+++ OpenCV3_Py_Examples/ImageW_R_WebCamera.py
@@ -11,33 +11,45 @@ Created on Mon Jul 10 15:58:39 2017
 """
 
 import cv2
-import numpy as np
+#import numpy as np
 
 from skimage import io
 
-fileStr = "C:\\Users\\yj_7u\\Pictures\\Icon.bmp"
+#fileStr = "C:\\Users\\yj_7u\\Pictures\\Icon.bmp"
 #fileStr = "C:\\Users\\006\\Pictures\\tx.jpg"
+fileStr = "C:\\Users\\006\\Pictures\\half.jpg"
 
 image = cv2.imread(fileStr)
-
 testImage = image.copy()
 
+
+image[0:50,:,2] = 255
+#cv2.waitKey()
+
+
+
+#----------------------------
+# Switch Blue Channel between Red Channel test,OpenCV Reversed
+testImage = image.copy()
 testImage[:,:,2] ^= testImage[:,:,0]
 testImage[:,:,0] ^= testImage[:,:,2] 
 testImage[:,:,2] ^= testImage[:,:,0]
 
-print(image[0])
-print("----------------------------------------------")
-print(testImage[0])
-
-io.imshow(image)
+#print(image[0])
+#print("----------------------------------------------")
+#print(testImage[0])
+#---------------------------
 
 io.imshow(testImage)
 
 
+print(image.shape)
+print(image.size)
+print(image.dtype)
 
-#cv2.imshow("TestImg", image)
-#cv2.waitKey()
+
+cv2.imshow("TestImg", image)
+cv2.waitKey()
 #cv2.imshow("TestImg", testImage)
 #cv2.waitKey()
-#cv2.destroyAllWindows()
+cv2.destroyAllWindows()
