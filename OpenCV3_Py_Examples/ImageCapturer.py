75468d3780972b806b2e6dd206bcf95852db5a44
ChopinNocture <chopin_7up@icloud.com>
2017-07-21 12:41:43 +0800

    卷积边缘检测测试

---
+++ OpenCV3_Py_Examples/ImageCapturer.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+
+OpenCV 3 学习示例
+
+Camera捕捉图像
+
+Created on Thu Jul 20 18:02:52 2017
+
+@author: yj_7u
+"""
+
+import cv2
+import time
+
+
+
+clicked = False
+
+def onMouse(evt, x, y, flags, param):
+    global clicked
+    if(evt==cv2.EVENT_LBUTTONDBLCLK):
+        clicked = True
+        
+        
+windowName = 'CameraShower'
+
+capturer = cv2.VideoCapture(0)
+
+cv2.namedWindow(windowName)
+cv2.setMouseCallback(windowName, onMouse)
+
+#suc = capturer.grab()
+#frame = capturer.retrieve()
+
+suc, frame=capturer.read()
+
+print(suc)
+#help(capturer.read)
+help(cv2.waitKey)
+while not clicked and suc:    
+#    frame = capturer.retrieve()
+#    suc = capturer.grab()
+    suc, frame=capturer.read()
+    
+    cv2.imshow(windowName, frame)
+    cv2.waitKey(20)
+
+
+capturer.release()
+cv2.destroyWindow(windowName)
--- OpenCV3_Py_Examples/ImageW_R_WebCamera.py
+++ OpenCV3_Py_Examples/ImageW_R_WebCamera.py
@@ -13,36 +13,59 @@ Created on Mon Jul 10 15:58:39 2017
 import cv2
 import time
 
-help(cv2.namedWindow)
-#import numpy as np
+#help(cv2.namedWindow)
+import numpy as np
 
 from skimage import io
+from scipy import ndimage
+
+kernel_3x3 = np.array([[-1, -1, -1],
+                       [-1,  8, -1],
+                       [-1, -1, -1]])
+
+kernel_5x5 = np.array([[-1, -1, -1, -1, -1],
+                       [-1,  1,  2,  1, -1],
+                       [-1,  2,  4,  2, -1],
+                       [-1,  1,  2,  1, -1],
+                       [-1, -1, -1, -1, -1]])
+
 
 #fileStr = "C:\\Users\\yj_7u\\Pictures\\Icon.bmp"
-fileStr = "C:\\Users\\006\\Pictures\\tx.jpg"
+fileStr = "C:\\Users\\yj_7u\\Pictures\\p2303767259.jpg"
+
+#fileStr = "C:\\Users\\006\\Pictures\\tx.jpg"
 #fileStr = "C:\\Users\\006\\Pictures\\half.jpg"
 
-image = cv2.imread(fileStr)
-testImage = image.copy()
+image = cv2.imread(fileStr, 0)
 
-i=0
-while i<100:
-    image[i,:,2] = 255
-    cv2.imshow('testimg', image)
-    print('' + str(time.time()) + ' ' + str(time.clock()))
-    
-    i+=1
-    cv2.waitKey(25)
-    #time.sleep(0.1)
+k3 = ndimage.convolve(image, kernel_3x3)
+k5 = ndimage.convolve(image, kernel_5x5)
 
-cv2.waitKey(0)
+blurred = cv2.GaussianBlur(image, (11,11), 0)
+bluredImg = image - blurred
 
+#i=0
+#while i<100:
+#    image[i,:,2] = 255
+#    cv2.imshow('testimg', image)
+#    print('' + str(time.time()) + ' ' + str(time.clock()))
+#    
+#    i+=1
+#    cv2.waitKey(25)
+#    #time.sleep(0.1)
+
+cv2.waitKey(-1)
+
+cv2.imshow("3x3", k3)
+cv2.imshow("5x5", k5)
+cv2.imshow("bluredImg", bluredImg)
 #----------------------------
 # Switch Blue Channel between Red Channel test,OpenCV Reversed
-testImage = image.copy()
-testImage[:,:,2] ^= testImage[:,:,0]
-testImage[:,:,0] ^= testImage[:,:,2] 
-testImage[:,:,2] ^= testImage[:,:,0]
+#testImage = image.copy()
+#testImage[:,:,2] ^= testImage[:,:,0]
+#testImage[:,:,0] ^= testImage[:,:,2] 
+#testImage[:,:,2] ^= testImage[:,:,0]
+cv2.waitKey(-1)
 
 #print(image[0])
 #print("----------------------------------------------")
@@ -56,7 +79,7 @@ print(image.size)
 print(image.dtype)
 
 
-#
+
 #clked = False
 #
 #def onMouse(evt, x, y, flags, param):
@@ -68,20 +91,20 @@ print(image.dtype)
 #
 #cv2.namedWindow('VideoWin')
 #cv2.setMouseCallback('VideoWin', onMouse)
-#
-#
+
+
 #
 #suc, frame = camCap.read()
 #
 #cv2.imshow('VideoWin', frame)
 #cv2.waitKey()
-#
+
 ##and cv2.waitKey(1)==-1 
 #while suc and not clked:
 #    cv2.imshow('VideoWin', frame)
 #    suc, frame = camCap.read()
 #    cv2.waitKey()
-
+#
 #cv2.imshow('VideoWin', frame)
 #cv2.waitKey()
 
